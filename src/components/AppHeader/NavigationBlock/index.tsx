602edebcaed0cba850625d642c33f82a4152596f
evaabsalyamova <absalyamovaeva@gmail.com>
2024-02-25 18:30:38 +0400

    Create `NavigationBlock` component

---
+++ src/components/AppHeader/NavigationBlock/index.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+
+
+const NavigationBlock = () => {
+    return (
+        <div className="navigationBlock">
+            <button className="films">FILMS</button>
+            <button className="series">SERIES</button>
+        </div>
+    )
+}
+
+export default NavigationBlock
--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import "./styles.css";
+import NavigationBlock from './NavigationBlock';
 
 const AppHeader = () => {
     return (
-       <div className='appHeader'>+++</div>
+        <div className='appHeader'>
+            <div className='appName'>
+               MOVIE
+            </div>
+            <NavigationBlock/>
+       </div>
     )
 }
 

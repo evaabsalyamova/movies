566afcbd9c8ba78f23b85948aa37c5ff026a5dac
evaabsalyamova <absalyamovaeva@gmail.com>
2024-04-08 14:36:28 +0400

    Add X-API-KEY in `api` component

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,10 +1,11 @@
-import { IExternalFilms } from "../types/movies";
+import { IExternalFilmsData } from "../types/movies";
 
 const headers = {
+  "X-API-KEY": "d565cdfc-210b-45be-8da0-5965ade46d28",
   "Content-type": "application/json; charset=UTF-8",
 };
 
-export const getFilms = async (): Promise<IExternalFilms> => {
+export const getFilms = async (): Promise<IExternalFilmsData> => {
   const response = await fetch(
     "https://kinopoiskapiunofficial.tech/api/v2.2/films",
     {

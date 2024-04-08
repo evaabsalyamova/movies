1caf80b1952facba42abc87c8aeeb7695fea434a
evaabsalyamova <absalyamovaeva@gmail.com>
2024-04-08 14:40:51 +0400

    Update external data types and mapper

--- src/components/AppHeader/NavigationBlock/FilmBlock/index.tsx
+++ src/components/AppHeader/NavigationBlock/FilmBlock/index.tsx
@@ -1,7 +1,25 @@
-const FilmBlock = () => {
-    return (
-        <div>+++</div>
-    )
-}
+import { useEffect } from "react";
+import { addInitialFilms } from "../../../../redux";
+import FilmList from "./FilmList";
+import { getFilms } from "../../../../api/movies";
+import { filmsMapper } from "../../../../types/movies";
+import { useDispatch } from "react-redux";
 
-export default FilmBlock
+const FilmBlock: React.FunctionComponent = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const filmList = getFilms();
+    if (filmList) {
+      filmList.then((data) => dispatch(addInitialFilms(filmsMapper(data))));
+    }
+  }, [dispatch]);
+
+  return (
+    <div className="filmBlockContainer">
+      <FilmList />
+    </div>
+  );
+};
+
+export default FilmBlock;
--- src/types/movies.ts
+++ src/types/movies.ts
@@ -1,22 +1,27 @@
 export interface IFilm {
   name: string;
+  id: number;
   year: number;
   poster: string;
 }
 
 export interface IExternalFilm {
-  nameEn: string;
+  nameOriginal: string;
+  kinopoiskId: number;
   year: number;
   posterUrlPreview: string;
 }
 
-export interface IExternalFilms {
+export interface IExternalFilmsData {
   items: IExternalFilm[];
+  total: number;
+  pages: number;
 }
 
-export const filmsMapper = (externalFilms: IExternalFilm[]): IFilm[] => {
-  return externalFilms.map((externalFilm) => ({
-    name: externalFilm.nameEn,
+export const filmsMapper = (externalFilms: IExternalFilmsData): IFilm[] => {
+  return externalFilms.items.map((externalFilm) => ({
+    name: externalFilm.nameOriginal,
+    id: externalFilm.kinopoiskId,
     year: externalFilm.year,
     poster: externalFilm.posterUrlPreview,
   }));

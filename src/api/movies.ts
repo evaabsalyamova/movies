d41a6a07c549015acac13b3db1655430844d06e8
evaabsalyamova <absalyamovaeva@gmail.com>
2024-05-06 14:41:49 +0400

    Add `getGenres` api method

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,4 +1,4 @@
-import { IExternalFilmsData } from "../types/movies";
+import { IExternalFilmsData, IFiltersData } from "../types/movies";
 
 const headers = {
   "X-API-KEY": "d565cdfc-210b-45be-8da0-5965ade46d28",
@@ -8,6 +8,19 @@ const headers = {
 export const getFilms = async (): Promise<IExternalFilmsData> => {
   const response = await fetch(
     "https://kinopoiskapiunofficial.tech/api/v2.2/films",
+    {
+      method: "GET",
+      headers,
+    }
+  )
+    .then((res) => res.json())
+    .then((json) => JSON.parse(JSON.stringify(json)));
+
+  return response;
+};
+export const getGenres = async (): Promise<IFiltersData> => {
+  const response = await fetch(
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/filters",
     {
       method: "GET",
       headers,

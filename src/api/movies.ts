66e1b015bbe05e7752738af52de3d3d40979a9bc
evaabsalyamova <absalyamovaeva@gmail.com>
2024-05-16 12:58:13 +0400

    Add `getFilmesByGenre` api method

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,4 +1,8 @@
-import { IExternalFilmsData, IFiltersData } from "../types/movies";
+import {
+  IExternalFilm,
+  IExternalFilmsData,
+  IFiltersData,
+} from "../types/movies";
 
 const headers = {
   "X-API-KEY": "d565cdfc-210b-45be-8da0-5965ade46d28",
@@ -21,6 +25,24 @@ export const getFilms = async (): Promise<IExternalFilmsData> => {
 export const getGenres = async (): Promise<IFiltersData> => {
   const response = await fetch(
     "https://kinopoiskapiunofficial.tech/api/v2.2/films/filters",
+    {
+      method: "GET",
+      headers,
+    }
+  )
+    .then((res) => res.json())
+    .then((json) => JSON.parse(JSON.stringify(json)));
+
+  return response;
+};
+
+export const getFilmesByGenre = async ({
+  genreId,
+}: {
+  genreId: number;
+}): Promise<IExternalFilm> => {
+  const response = await fetch(
+    `https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=${genreId}`,
     {
       method: "GET",
       headers,
--- src/components/AppHeader/NavigationBlock/FilmBlock/FilmList/GenresItem/index.tsx
+++ src/components/AppHeader/NavigationBlock/FilmBlock/FilmList/GenresItem/index.tsx
@@ -1,10 +1,18 @@
+import { getFilmesByGenre } from "../../../../../../api/movies";
 import { IGenre } from "../../../../../../types/movies";
 
 interface IProps {
   data: IGenre;
 }
 const GenresItem: React.FunctionComponent<IProps> = ({ data }) => {
-  return <div className="genre">{data.genre}</div>;
+  return (
+    <button
+      className="genre"
+      onClick={() => getFilmesByGenre({ genreId: data.id })}
+    >
+      {data.genre}
+    </button>
+  );
 };
 
 export default GenresItem;

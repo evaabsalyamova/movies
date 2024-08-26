1901ab0456b9490cee2738ab2410500b253645e0
evaabsalyamova <absalyamovaeva@gmail.com>
2024-08-26 22:14:29 +0400

    Get detailed film data for `FilmPage`

--- src/components/AppHeader/NavigationBlock/FilmPage/index.tsx
+++ src/components/AppHeader/NavigationBlock/FilmPage/index.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 import { useParams } from "react-router-dom";
+import { getFilmByID } from "../../../../api/movies";
+import { IDetailedFilm, filmDetailedMapper } from "../../../../types/movies";
 
-const FilmPage = () => {
+const FilmPage: React.FunctionComponent = () => {
   const { id } = useParams();
+  const [film, setFilm] = useState<undefined | IDetailedFilm>();
 
-  return <>{id}</>;
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    const filmPromise = getFilmByID({ filmId: Number(id) });
+    filmPromise.then((data) => setFilm(filmDetailedMapper(data)));
+  }, [id]);
+
+  if (!film) {
+    return <>film is not available</>;
+  }
+  return (
+    <div className="filmPage">
+      <div className="posterBlock"></div>
+
+      <div className="filmInfoBlock">
+        <div className="filmName">{film.name}</div>
+        <div className="filmYear">{film.year}</div>
+      </div>
+    </div>
+  );
 };
 
 export default FilmPage;
